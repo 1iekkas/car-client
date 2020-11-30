1a2b5bdc6acd02f5271eccdf408506c533245a87
1iekkas <764295062@qq.com>
2020-11-30 18:26:40 +0800

    更新车辆列表接口

---
+++ router-note.js
@@ -0,0 +1,21 @@
+/**
+ * pages    主入口
+ *  ——map    首页地图
+ *  ——user    用户中心
+ **/
+
+/**
+ * userPackage    用户模块
+ *  ——userCar    用户车辆列表
+ *  ——carInfo    车辆详细信息
+ *  ——carList    车辆选择列表
+ *  ——citySearch    城市切换列表
+ *  ——order    用户订单列表
+ *  ——login    登录
+ *  ——mapSearch    位置搜索
+ */
+
+/**
+ * servicePackage    业务逻辑模块
+ * 
+ */
